488f7d55e8743e864b55b1e18eabee4217e2ac8b
Leandro Silva <leandrodoze@gmail.com>
2019-08-25 15:28:46 -0300

    working on hadling

--- server/handle_read.go
+++ server/handle_read.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 var (
@@ -10,10 +12,36 @@ var (
 )
 
 func handleRead(w http.ResponseWriter, r *http.Request) {
-	key := r.URL.Path[len("/read/"):]
+	path := splitPath(r.URL)
+	articleKey := path[0]
 
+	if articleKey == "" {
+		http.Error(w, "Missing article key", http.StatusBadRequest)
+		return
+	}
+
+	if len(path) > 1 && path[1] == "raw" {
+		handleReadRaw(w, r, articleKey)
+		return
+	}
+
+	handleReadProcessed(w, r, articleKey)
+}
+
+func handleReadProcessed(w http.ResponseWriter, r *http.Request, articleKey string) {
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(key))
+	w.Write([]byte("processed-"))
+	w.Write([]byte(articleKey))
+}
+
+func handleReadRaw(w http.ResponseWriter, r *http.Request, articleKey string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("raw-"))
+	w.Write([]byte(articleKey))
+}
+
+func splitPath(url *url.URL) []string {
+	return strings.Split(url.Path[len("/read/"):], "/")
 }
 
 func getReadURL() string {
